373ee74fd44dc1b40044f55c47aae1a6fda51991
Francisco <montejof1998@gmail.com>
2023-08-28 22:15:20 -0700

    the data is displaying when the request is send

--- services/productService.js
+++ services/productService.js
@@ -1,24 +1,10 @@
 import { faker } from "@faker-js/faker";
 import { v4 as uuidv4 } from "uuid";
+import { data } from "../data.js";
 
 class ProductService {
   constructor() {
-    this.products = [];
-    this.generate();
-  }
-
-  generate() {
-    const limit = 20;
-    for (let index = 0; index < limit; index++) {
-      this.products.push({
-        id: uuidv4(),
-        name: faker.commerce.productName(),
-        price: parseInt(faker.commerce.price()),
-        category: faker.commerce.department(),
-        Image: faker.image.url(),
-        isBlock: faker.datatype.boolean(),
-      });
-    }
+    this.products = data;
   }
 
   async create(data) {
