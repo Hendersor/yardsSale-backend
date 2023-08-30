2f1dd1fe9c6c748bdd34c77e24d628c5d2d204ef
Francisco <montejof1998@gmail.com>
2023-08-29 18:00:28 -0700

    wish list service is done and working

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,14 @@
 import express from "express";
 import { router as productsRouter } from "./productsRouter.js";
 import { router as cartRouter } from "./cartRouter.js";
+import { router as wishRouter } from "./wishListRouter.js";
 
 function routerApi(app) {
   const router = express.Router();
   app.use("/api/v1", router);
   router.use("/products", productsRouter);
   router.use("/cart", cartRouter);
+  router.use("/wish", wishRouter);
 }
 
 export { routerApi };
