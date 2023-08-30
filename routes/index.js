98b34650a1b0c38bc857ffd1f586baf09eb82a99
Francisco <montejof1998@gmail.com>
2023-08-29 22:57:42 -0700

    working on the services for the users

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import { router as productsRouter } from "./productsRouter.js";
 import { router as cartRouter } from "./cartRouter.js";
 import { router as wishRouter } from "./wishListRouter.js";
+import { router as usersRouter } from "./usersRouter.js";
 
 function routerApi(app) {
   const router = express.Router();
@@ -9,6 +10,7 @@ function routerApi(app) {
   router.use("/products", productsRouter);
   router.use("/cart", cartRouter);
   router.use("/wish", wishRouter);
+  router.use("/users", usersRouter);
 }
 
 export { routerApi };
